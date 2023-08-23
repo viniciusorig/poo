b7382a54fe7fe241f19f12149bc7a455bcef94fd
Your Name <viniciusbarbosaribeiro12@gmail.com>
2023-08-23 09:56:53 -0400

    em tese pronto

--- bank.py
+++ bank.py
@@ -21,21 +21,28 @@ class Banco:
     def saque(self, cliente, valor)->bool:
         if(cliente.saldo == 0):
             print("Nao saldo eh zero nao eh possivel realizar o saque")
+            cliente.operacao.append(f"Saque nao efetudo :: {date.today()}")
             return False
         if((cliente.saldo - valor) < 0):
             print("nao sera possivel realizar o saque pois seu saldo ficaria negativo")
+            cliente.operacao.append(f"Saque nao efetudo :: {date.today()}")
             return False
         cliente.saldo -= valor
+        cliente.operacao.append(f"Saque efetudo :: {date.today()}")
         return True
     
     def deposito(self, cliente, valor)->bool:
         cliente.saldo += valor
+        cliente.operacao.append(f"deposito efetudo :: {date.today()}")
         return True
     
-    def tranferencia(self, cliente_orig, cliente_dest, valor)->True:
+    def tranferencia(self, cliente_orig, cliente_dest, valor)->bool:
         if(self.saque(cliente_orig, valor) == False):
             print("naao foi possivel realizar a tranferencia pois a conta de origem nao tem saldo")
+            return False
         self.deposito(cliente_dest, valor)
+        cliente_orig.operacao.append(f"tranferencia efetuada para :: {cliente_dest} horario:: {date.today()}")
+        return True
 
 class Conta:
     def __init__(self, agencia, numero, saldo, cliente:'Cliente'):
@@ -44,7 +51,9 @@ class Conta:
         self.saldo = saldo
         self.cliente = cliente
         self.__repr__()
-    def __repr__(self) -> str:
+        self.operacaao = []
+
+   def __repr__(self) -> str:
        print(f'''Agencia: {self.agencia}\nNumero: {self.numero}\nSaldo_atual: {self.saldo}''')
 
 c1 = Cliente('vini','052', date(2002, 9, 12))
